1c0ad3800091e3aa00ffa584eb0e44656c35068f
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-03 16:00:02 -0400

    added a readme.md

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Memory-Card-Game
+
+This is a simple memory card game implemented using HTML, CSS, and JavaScript. In this game, players need to match pairs of animal emojis by flipping cards.
+
+How to Play
+Click on a card to reveal the animal emoji.
+Click on another card to find its matching pair.
+If the two cards match, they stay flipped.
+If the cards don't match, they will be flipped back after a short delay.
+Continue flipping cards and matching pairs until all pairs are found.
+Technologies Used
+HTML: Provides the structure of the game.
+CSS: Styles the game interface and cards.
+JavaScript: Implements the game logic and interactivity.
+Game Features
+Responsive Design: The game interface is responsive and can be played on various devices.
+Randomized Cards: The animal emojis are randomly shuffled every time the game starts or restarts.
+Restart Button: Players can restart the game at any time by clicking the "Restart Game" button.
+Card Flipping Animation: Cards flip with a smooth animation to enhance the user experience.
+How to Run the Game Locally
+Clone this repository to your local machine.
+Open the index.html file in your web browser to play the game.
+Have fun playing the Memory Card Game! If you encounter any issues or have suggestions for improvements, feel free to contribute or reach out to us.
+
+Demo: https://memory-card-game-vanilla.netlify.app/
--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const animals = ['🐿️', '🐪', '🐈', '🐙', '🐝', '🦇', '🐸', '🦍']; // list of animal emojis written on different cards.
+const animals = ['🐿️', '🐪', '🐈', '🐙', '🐝', '🦇', '🐸', '🦍']; // list of animal emojis 
 const gameGrid = animals.concat(animals).sort(() => 0.5 - Math.random()); // shuffle and duplicate the cards to create a mixed deck for the game.
 let firstCard = null; //create two empty slots (variables) 
 let secondCard = null; 
@@ -7,21 +7,21 @@ const memoryGame = document.getElementById('memory-game'); // (HTML element)
 
 function createCard(animal, index) { //create a template for a single game card with an animal emoji and an index number.
     const card = document.createElement('div'); // html element
-    card.classList.add('card');
-    card.dataset.animal = animal;
-    card.dataset.index = index;
+    card.classList.add('card'); //marks the HTML element as a card element for styling and functionality.
+    card.dataset.animal = animal; // each card can hold a different animal
+    card.dataset.index = index; //assigning a number to a card
 
     const frontFace = document.createElement('div'); // html element
-    frontFace.classList.add('front-face');
+    frontFace.classList.add('front-face'); //It's like keeping the front of the card hidden until it's flipped
     frontFace.textContent = '?';
     card.appendChild(frontFace);
 
     const backFace = document.createElement('div');
-    backFace.classList.add('back-face');
-    backFace.textContent = animal;
-    card.appendChild(backFace);
+    backFace.classList.add('back-face'); //This label helps style and identify the back side separately from the front side.
+    backFace.textContent = animal; //setting the content of the back side to an animal emoji
+    card.appendChild(backFace); 
 
-    card.addEventListener('click', flipCard);
+    card.addEventListener('click', flipCard); //eventlistener
     return card;
 }
 
@@ -40,12 +40,12 @@ function flipCard() { //When a player clicks on a card, you flip it over to reve
 }
 
 function checkForMatch() { //You compare the animals on the first and second flipped cards to see if they match.
-    const isMatch = firstCard.dataset.animal === secondCard.dataset.animal;
-    isMatch ? disableCards() : unflipCards();
+    const isMatch = firstCard.dataset.animal === secondCard.dataset.animal; // checking if the 1sr card is the same as 2nd
+    isMatch ? disableCards() : unflipCards(); // if cards match-great if not- they close and we keep choosing other crads
 }
 
-function disableCards() { //If the cards match, you remove the ability to click on those specific cards.
-    firstCard.removeEventListener('click', flipCard);
+function disableCards() { //removing click event
+    firstCard.removeEventListener('click', flipCard); 
     secondCard.removeEventListener('click', flipCard);
     resetBoard();
 }
@@ -55,7 +55,7 @@ function unflipCards() { //If the cards don't match, you hide the animals again 
         firstCard.classList.remove('flipped');
         secondCard.classList.remove('flipped');
         resetBoard();
-    }, 600); // Delay set to 1000 milliseconds (1 second)
+    }, 600); // Delay flip for less then a second
 }
 
 function resetBoard() { //You clear the slots, making them ready for the next pair of flipped cards.
@@ -69,10 +69,10 @@ function restartGame() { //You reshuffle the cards and reset the game to its ini
 }
 
 function initGame() { //You set up the game by creating and arranging the cards on the table.
-    gameGrid.forEach((animal, index) => {
-        const card = createCard(animal, index);
-        memoryGame.appendChild(card);
-    });
+    gameGrid.forEach((animal, index) => { // For each animal in the game grid,
+        const card = createCard(animal, index); //create a card using the createCard function, providing the animal and its index
+        memoryGame.appendChild(card); //place this card on the table (memoryGame) to set up the game
+    }); 
 }
 
 initGame(); //You start the game as soon as everything is set up.
